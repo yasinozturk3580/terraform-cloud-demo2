081dc6e87b532726a13a70515fea58b13b3ab88a
yasinozturk3580 <128163154+yasinozturk3580@users.noreply.github.com>
2023-06-22 15:36:01 -0500

    added asg sec

--- asg/main.tf
+++ asg/main.tf
@@ -26,7 +26,8 @@ module "asg" {
 resource "aws_elb" "bar" {
   name = "foobar-terraform-elbs"
   availability_zones = data.terraform_remote_state.vpc.outputs.azs
-  security_groups = [aws_security_group.lb-firewall.]
+  security_groups = [aws_security_group.lb-firewall.id]
+    
 
 
 
