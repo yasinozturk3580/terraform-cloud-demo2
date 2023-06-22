4637acf0674333398f7b0a3a3d77dfaf0f2502f2
yasinozturk3580 <128163154+yasinozturk3580@users.noreply.github.com>
2023-06-22 15:22:22 -0500

    added sec group

--- asg/main.tf
+++ asg/main.tf
@@ -52,4 +52,32 @@ health_check {
 
 }
   
-  
+resource "aws_security_group" "lb-firewall" {
+  name        = "lb-firewall"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}  
